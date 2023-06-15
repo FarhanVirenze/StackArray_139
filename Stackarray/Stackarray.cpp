501f395584900298f1e7ef573152b95c7af1087f
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-15 15:08:08 +0700

    tambahan void pop

--- Stackarray/Stackarray.cpp
+++ Stackarray/Stackarray.cpp
@@ -2,12 +2,54 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class StackArray {
+	string stack_array[5];
+	int top;
+public:
+	//constructor
+	StackArray() {
+		top = -1;
+	}
+	
+	string push(string element) {
+		if (top == 4) { //step 1
+			cout << "Number of data exceeds the limit." << endl;
+			return "";
+		}
 
+		top++; //step 2
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << "ditambahkan(pushed)" << endl;
+
+		return element;
+	}
+
+	void pop() {
+		if (empty()) { //step 1
+			cout << "\nStack is empty. Cannot pop." << endl; // 1.a
+			return; //1.b
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl; //step 2
+		top--; //step 3 decrement
+	}
+
+	//method for check if data is empty
+	bool empty() {
+		return (top == -1);
+	}
+
+	void display() {
+		if (empty()) {
+			cout << "\nStack is empty." << endl;
+		}
+		else
+	}
+};
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
