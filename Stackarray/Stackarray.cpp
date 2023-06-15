a293e9f63dca07dc737d50cc71408aa0edee600a
FarhanVirenze <farhanvirenze18@gmail.com>
2023-06-15 15:14:24 +0700

    tambahan fungsi string input

--- Stackarray/Stackarray.cpp
+++ Stackarray/Stackarray.cpp
@@ -58,7 +58,14 @@ public:
 int main() {
 	StackArray s;
 	while (true) {
-
+		cout << endl;
+		cout << "\n***Stack Menu***\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "\nEnter your choice: ";
+		string input;
 	}
 }
 
